8a9d0087073abdbf1b35e3393a0c5a337f243b41
ziss05 <45897789+ziss05@users.noreply.github.com>
2023-01-15 08:26:21 +0100

    refactor long method updateEdit()

--- src/main/java/project_16x16/objects/EditableObject.java
+++ src/main/java/project_16x16/objects/EditableObject.java
@@ -15,157 +15,166 @@ import project_16x16.scene.GameplayScene;
  */
 public abstract class EditableObject extends PClass {
 
-	// Base Data
-	public PVector position;
-	public int width;
-	public int height;
+    // Base Data
+    public PVector position;
+    public int width;
+    public int height;
 
-	// Image data
-	public String id;
+    // Image data
+    public String id;
 
-	enum type {
-		COLLISION, BACKGROUND, OBJECT
-	}
+    enum type {
+        COLLISION, BACKGROUND, OBJECT
+    }
 
-	protected type type;
+    protected type type;
 
-	// Focus
-	private boolean focus;
+    // Focus
+    private boolean focus;
 
-	/**
-	 * Child of gameObject.
-	 */
-	public boolean child;
+    /**
+     * Child of gameObject.
+     */
+    public boolean child;
 
-	// Map Editor Scene
-	public GameplayScene gameplayScene;
+    // Map Editor Scene
+    public GameplayScene gameplayScene;
 
-	protected PVector editOffset;
+    protected PVector editOffset;
 
-	public EditableObject(SideScroller sideScroller, GameplayScene gameplayScene) {
-		super(sideScroller);
+    public EditableObject(SideScroller sideScroller, GameplayScene gameplayScene) {
+        super(sideScroller);
 
-		position = new PVector(0, 0);
-		editOffset = new PVector(0, 0);
-		this.gameplayScene = gameplayScene;
-	}
+        position = new PVector(0, 0);
+        editOffset = new PVector(0, 0);
+        this.gameplayScene = gameplayScene;
+    }
 
-	public abstract void display();
+    public abstract void display();
 
-	public abstract void debug();
+    public abstract void debug();
 
-	public abstract JSONObject exportToJSON();
+    public abstract JSONObject exportToJSON();
 
-	/**
-	 * Draws position edit arrows and bounding box if the object is selected
-	 * (focused) in MODIFY mode.
-	 */
-	public void displayEdit() {
-		if (focus) { // focus = held by player
-			// draw border around object
-			applet.strokeWeight(10);
-			applet.noFill();
-			applet.stroke(0, 255, 200);
-			applet.rect(position.x, position.y, width, height);
-			applet.strokeWeight(4); // reset style
-		}
-	}
+    /**
+     * Draws position edit arrows and bounding box if the object is selected
+     * (focused) in MODIFY mode.
+     */
+    public void displayEdit() {
+        if (focus) { // focus = held by player
+            // draw border around object
+            applet.strokeWeight(10);
+            applet.noFill();
+            applet.stroke(0, 255, 200);
+            applet.rect(position.x, position.y, width, height);
+            applet.strokeWeight(4); // reset style
+        }
+    }
 
-	/**
-	 * Called during modify mode when mouse pressed
-	 */
-	public void updateEdit() {
-		if (child) {
-			return;
-		}
-		if (applet.mouseReleaseEvent && applet.mouseButton == LEFT) {
-			focus = false;
-			return;
-		}
+    /**
+     * Called during modify mode when mouse pressed
+     */
+    public void updateEdit() {
+        if (child) {
+            return;
+        }
+        if (applet.mouseReleaseEvent && applet.mouseButton == LEFT) {
+            focus = false;
+            return;
+        }
+        // Focus Event
+        focusEvent();
+    }
 
-		// Focus Event
-		if (applet.mousePressEvent && applet.mouseButton == LEFT && !focus) {
-			if (mouseHover()) { // Focus Enable
-				if (gameplayScene.focusedObject == null) {
-					focus = true;
-					gameplayScene.focusedObject = this;
-				}
-			}
-			else { //Franziska: never reachable or need focus=false and then focus true/ oder hier weil different threads?
-				if (focus) { // Focus Disable
-					gameplayScene.focusedObject = null;
-					focus = false;
-				}
-			}
-		}
-		if (focus) { // When Focused
-			if (applet.mousePressEvent) {
-				if (mouseHover()) {
-					focus = true;
-					editOffset = PVector.sub(position, applet.getMouseCoordGame());
-				}
-			}
+    private void focusEvent() {
+        focusOnMouseIfLeftClick();
+        if (focus) { // When Focused
+            if (applet.mousePressEvent && mouseHover()) {
+                focus = true;
+                editOffset = PVector.sub(position, applet.getMouseCoordGame());
+            }
+            // Duplicate Object Shift
+            duplicateObjectShift();
 
-			// Duplicate Object Shift
-			if (applet.keyPressEvent && applet.isKeyDown(SideScroller.SHIFT)) {
-				EditableObject copy; // Duplicate Instance
-				switch (type) {
-					case COLLISION:
-						copy = new CollidableObject(applet, gameplayScene, id, 0, 0);
-						copy.focus = true;
-						copy.position = position.copy();
-						copy.editOffset = editOffset.copy();
-						gameplayScene.objects.add(copy);
-						break;
-					case OBJECT:
-						try {
-							Class<? extends GameObject> gameObjectClass = Tileset.getObjectClass(id);
-							Constructor<?> ctor = gameObjectClass.getDeclaredConstructors()[0];
-							copy = (GameObject) ctor.newInstance(new Object[] { applet, this });
-							copy.focus = true;
-							copy.position = position.copy();
-							copy.editOffset = editOffset.copy();
-							gameplayScene.objects.add(copy);
-							break;
-						}
-						catch (Exception e) {
-							e.printStackTrace();
-						}
-						switch (id) {
-							case "MIRROR_BOX":
-								((MirrorBoxObject) gameplayScene.objects.get(gameplayScene.objects.size() - 1)).direction = ((MirrorBoxObject) this).direction;
-								break;
-						}
-						break;
-					default:
-						break;
-				}
-				applet.keyPressEvent = false;
-				focus = false;
-			}
-			if (focus && applet.mousePressed && applet.mouseButton == LEFT) {
-				position = new PVector(Utility.roundToNearest(applet.getMouseCoordGame().x + editOffset.x, SideScroller.snapSize), Utility.roundToNearest(applet.getMouseCoordGame().y + editOffset.y, SideScroller.snapSize));
-			}
-		}
-	}
+            if (focus && applet.mousePressed && applet.mouseButton == LEFT) {
+                position = new PVector(Utility.roundToNearest(applet.getMouseCoordGame().x + editOffset.x, SideScroller.snapSize), Utility.roundToNearest(applet.getMouseCoordGame().y + editOffset.y, SideScroller.snapSize));
+            }
+        }
+    }
 
-	public void focus() {
-		editOffset = PVector.sub(position, applet.getMouseCoordGame());
-		focus = true;
-	}
+    private void duplicateObjectShift() {
+        if (applet.keyPressEvent && applet.isKeyDown(SideScroller.SHIFT)) {
+            switch (type) {
+                case COLLISION:
+                    handleCollision();
+                    break;
+                case OBJECT:
+                    handleObject();
+                    break;
+                default:
+                    break;
+            }
+            applet.keyPressEvent = false;
+            focus = false;
+        }
+    }
 
-	public void unFocus() {
-		focus = false;
-	}
+    private void handleObject() {
+        EditableObject copy;
+        try {
+            Class<? extends GameObject> gameObjectClass = Tileset.getObjectClass(id);
+            Constructor<?> ctor = gameObjectClass.getDeclaredConstructors()[0];
+            copy = (GameObject) ctor.newInstance(new Object[]{applet, this});
+            copy.focus = true;
+            copy.position = position.copy();
+            copy.editOffset = editOffset.copy();
+            gameplayScene.objects.add(copy);
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if ("MIRROR_BOX".equals(id)) {
+            ((MirrorBoxObject) gameplayScene.objects.get(gameplayScene.objects.size() - 1)).direction = ((MirrorBoxObject) this).direction;
+        }
+    }
 
-	public boolean isFocused() {
-		return focus;
-	}
+    private void handleCollision() {
+        EditableObject copy;
+        copy = new CollidableObject(applet, gameplayScene, id, 0, 0);
+        copy.focus = true;
+        copy.position = position.copy();
+        copy.editOffset = editOffset.copy();
+        gameplayScene.objects.add(copy);
+    }
 
-	public boolean mouseHover() {
-		if (applet.mouseX < 400 && applet.mouseY < 100) { // Over Inventory Bar -- rough approximation
-			return false;
-		}
-		return Utility.hoverGame(position.x, position.y, width, height);
-	}
+    private void focusOnMouseIfLeftClick() {
+        if (applet.mousePressEvent && applet.mouseButton == LEFT && !focus && mouseHover()) {
+            // Focus Enable
+            if (gameplayScene.focusedObject == null) {
+                focus = true;
+                gameplayScene.focusedObject = this;
+
+            }
+        }
+    }
+
+    public void focus() {
+        editOffset = PVector.sub(position, applet.getMouseCoordGame());
+        focus = true;
+    }
+
+    public void unFocus() {
+        focus = false;
+    }
+
+    public boolean isFocused() {
+        return focus;
+    }
+
+    public boolean mouseHover() {
+        if (applet.mouseX < 400 && applet.mouseY < 100) { // Over Inventory Bar -- rough approximation
+            return false;
+        }
+        return Utility.hoverGame(position.x, position.y, width, height);
+    }
 }
